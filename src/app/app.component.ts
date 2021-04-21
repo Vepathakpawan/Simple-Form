14be64812a59b147bf50d20db1d99870e7c189de
Vepathakpawan <pawanved007@gmail.com>
2021-04-21 03:59:14 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import{FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Simpleform';
-}
+  info;
+  constructor(private formBuilder: FormBuilder){
+  this.info = this.formBuilder.group({
+  name: '',
+  email: '',
+  contact: '',
+  address:'',
+  age:'',
+  gender:'',
+  
+  })
+  }
+  onSubmit(values:any){
+  console.warn("Form Submit", values)
+  }
+  }
